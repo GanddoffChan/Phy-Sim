dcb336b3f37a1410e7c1da320d9952cbe5cec663
Gordon Chan <48621315+GanddoffChan@users.noreply.github.com>
2019-06-20 12:05:48 +0800

    Add files via upload

---
+++ Simple Pendulum.py
@@ -0,0 +1,52 @@
+from tkinter import *
+import time
+import math
+
+root = Tk()
+
+canvas_width = 100
+canvas_height = 100
+C = Canvas(root, width=canvas_width, height=canvas_height, background='black')
+C.pack()
+
+X = int(canvas_width / 2)
+Y = int(canvas_height / 2)
+
+t = 0
+
+omega = 0.005
+
+angle = 90
+
+theta = angle*math.pi/180
+
+if X > Y:
+    R = Y-10
+
+else:
+    R = X-10
+
+r = 5
+k = 0.00001
+gamma = 0
+
+rate = 0.0000005
+
+while True:
+    C.delete("all")
+    alpha = -rate*math.sin(theta)-gamma
+    omega += alpha
+    theta += omega
+
+    gamma = k*omega
+    
+    x = X + R*math.sin(theta)
+    y = Y + R*math.cos(theta)
+    C.create_line(x,y, X,Y, fill="white")
+    C.create_oval(x-r,y-r, x+r,y+r, fill='white')
+    C.update()
+    
+root.mainloop()
+
+
+
