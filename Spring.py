6675ef2f5494504770e283a7f5b25bb7defdac8f
Gordon Chan <48621315+GanddoffChan@users.noreply.github.com>
2019-06-21 17:17:23 +0800

    Update Spring.py

--- Spring.py
+++ Spring.py
@@ -1,27 +1,44 @@
 from tkinter import *
 
 W = 400
-H = 900
+H = 500
 
 root = Tk()
 root.title('Spring')
 C = Canvas(root, width=W, height=H, background='white')
+spring = Scale(root, label='k', from_=1,to=10, length=W,
+               tickinterval=10, orient=HORIZONTAL)
+mass = Scale(root, label='m', from_=1,to=10, length=W,
+               tickinterval=10, orient=HORIZONTAL)
+length = Scale(root, label='l', from_=100,to=250, length=W,
+               tickinterval=10, orient=HORIZONTAL)
+gravity = Scale(root, label='g', from_=-1,to=-10, length=W,
+               tickinterval=10, orient=HORIZONTAL)
+damping = Scale(root, label='c', from_=1,to=10, length=W,
+               tickinterval=10, orient=HORIZONTAL)
+spring.pack()
+mass.pack()
+length.pack()
+gravity.pack()
+damping.pack()
 C.pack()
 
-k = 10
-m = 10
-l = 450
-h = 400
-g = -9.81
-v = -0.1
+h = 20
+v = 0
 
 rate = 10**-5
 
 while True:
     C.delete('all')
 
+    k = spring.get()
+    m = mass.get()
+    l = length.get()
+    g = gravity.get()
+    c = damping.get()
+    
     Δy = H -l -h
-    a = (g +k*Δy/m)*rate
+    a = (g +k*Δy/m -c*v/m)*rate
     v += a
     h += v
 
@@ -35,4 +52,4 @@ while True:
     C.create_line(x+w/2,(H+y)/2, x-w/2,H, width=2)
     C.update()
 
-root.mainloop()    
+root.mainloop()
