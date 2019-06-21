14f79918b212e5a0ffac3a74e26ed458850d9f53
Gordon Chan <48621315+GanddoffChan@users.noreply.github.com>
2019-06-21 09:34:28 +0800

    Update Simple Pendulum.py

--- Simple Pendulum.py
+++ Simple Pendulum.py
@@ -1,44 +1,39 @@
 from tkinter import *
 from math import *
 
+S = 500
+O = S/2
+r = 0.05*O
+
 root = Tk()
-W = 500
-H = 500
-C = Canvas(root, width=W, height=H, background='black')
+C = Canvas(root, width=S, height=S, background='black')
+length = Scale(root, label='L', from_=.1*O,to=.9*O, length=S, tickinterval=50, orient=HORIZONTAL)
+damping = Scale(root, label='γ', from_=0,to=100, length=S, tickinterval=50, orient=HORIZONTAL)
 C.pack()
+length.pack()
+damping.pack()
 
-X = int(W/2)
-Y = int(H/2)
-
-if X > Y:
-    R = .9*Y
-
-else:
-    R = .9*X
-
-r = 0.05*R    
-
-ω = 2*10**-3
-
-angle = 90
+ω = 1*10**-5
+angle = 180
 θ = angle*pi/180
-
-γ = 1*10**-5
-
-rate = 5*10**-7
+g = 5*10**-5
 
 while True:
     C.delete('all')
+
+    L = length.get()
+    γ = damping.get()*10**-6
     
-    α = -rate*sin(θ)-γ*ω
+    α = -g/L*sin(θ) -γ*ω
     ω += α
     θ += ω
     
-    x = X + R*sin(θ)
-    y = Y + R*cos(θ)
+    x = O + L*sin(θ)
+    y = O + L*cos(θ)
     
-    C.create_line(x,y, X,Y, fill="white")
-    C.create_oval(x-r,y-r, x+r,y+r, fill='white')
+    C.create_line(x,y, O,O, fill='white', width=2)
+    C.create_oval(x-r,y-r, x+r,y+
+                  r, fill='black', outline='white', width=2)
     C.update()
     
 root.mainloop()
