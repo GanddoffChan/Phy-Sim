9fccdabf9bb4b6d41a9fc63b20af17a59a59b951
Gordon Chan <48621315+GanddoffChan@users.noreply.github.com>
2019-06-21 09:56:38 +0800

    Update Brownian Turtle.py

--- Brownian Turtle.py
+++ Brownian Turtle.py
@@ -2,9 +2,8 @@ from random import *
 from turtle import *
 bgcolor('dim grey')
 
-##These are the boundaries for turtle movement. 
-bx = 400
-by = 400
+##These is the size of the boundary for turtle movement. 
+BS = 400
 
 turtle = Turtle()
 turtle.shape('turtle')
@@ -12,14 +11,17 @@ turtle.pencolor('white')
 
 while True:
     s = randint(0, 100)
-    r = randint(0,359)
+    x = randint(0,359)
 ##To increase the probability of moving forward.    
-    θ = (1/90)*(r**2)-4*r+360
+    θ = (1/90)*(x**2) -4*x +360
     
     turtle.left(θ)
     turtle.forward(s)
 
-    if turtle.xcor() < -bx or turtle.xcor() > bx or turtle.ycor() < -by or turtle.ycor() > by:
+    if    (turtle.xcor() < -BS
+        or turtle.ycor() < -BS
+        or turtle.xcor() > BS
+        or turtle.ycor() > BS):
 
         turtle.left(180)
         turtle.forward(s)    
