cadf0aebb170ba351e45358d3f8099b294d9189d
Gordon Chan <48621315+GanddoffChan@users.noreply.github.com>
2019-06-21 13:04:25 +0800

    Update Simple Pendulum.py

--- Simple Pendulum.py
+++ Simple Pendulum.py
@@ -9,24 +9,32 @@ root = Tk()
 C = Canvas(root, width=S, height=S, background='black')
 length = Scale(root, label='L', from_=.1*O,to=.9*O, length=S,
                tickinterval=O/10, orient=HORIZONTAL)
-damping = Scale(root, label='γ', from_=0,to=100, length=S,
-                tickinterval=10, orient=HORIZONTAL)
+gravity = Scale(root, label='g', from_=0,to=-10, length=S,
+                tickinterval=1, orient=HORIZONTAL, showvalue=0)
+torque = Scale(root, label='τ', from_=-5,to=5, length=S,
+                tickinterval=1, orient=HORIZONTAL, showvalue=0)
+damping = Scale(root, label='γ', from_=0,to=10, length=S,
+                tickinterval=1, orient=HORIZONTAL, showvalue=0)
 C.pack()
 length.pack()
+gravity.pack()
+torque.pack()
 damping.pack()
 
-angle = 180
+angle = 30
 θ = angle*pi/180
-ω = 1*10**-5
-g = 5*10**-5
+ω = 0
 
 while True:
     C.delete('all')
 
     L = length.get()
-    γ = damping.get()*10**-6
+    g = gravity.get()*10**-4
+    τ = torque.get()*10**-2
+    γ = damping.get()*10**-8
+
     
-    α = -g/L*sin(θ) -γ*ω
+    α = g/L*sin(θ) +τ/L**2 -γ*(ω*L)**2
     ω += α
     θ += ω
     
